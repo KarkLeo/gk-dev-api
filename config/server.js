436661b486df3c5c362e446e5568ffe47c5fa0ba
bylko.aleksey <vyacheslav.malykhin@anuitex.net>
2021-06-08 17:59:29 +0300

    Strapi init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8d5ff014f5c37254457d04335b61a3ed'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4885f6e2-1e2e-4af0-9061-91f70147de93'
+};
