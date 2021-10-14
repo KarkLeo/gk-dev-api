69f0eccf228479bd235e2178056b8781964ceb4c
slava-malykhin <slava.malykhin@placense.com>
2021-10-14 21:03:38 +0300

    Fixed bot text

--- api/front-refresh/controllers/front-refresh.js
+++ api/front-refresh/controllers/front-refresh.js
@@ -35,7 +35,7 @@ ${data.cart_item.map(i => `${i.product.vendor_code}${'.'.repeat(12 - i.product.v
 Информация о доставке:
 ${data.delivery_info.first_name} ${data.delivery_info.last_name}
 ${data.delivery_info.phone_number}
-${data.delivery_info.is_novaposhta ? data.delivery_info.city + ` Отдклкние № ${data.delivery_info.novaposhta_number}` : data.delivery_info.address}
+${data.delivery_info.is_novaposhta ? data.delivery_info.city + ` Отделение № ${data.delivery_info.novaposhta_number}` : data.delivery_info.address}
 
 Комментарий к заказу:
 ${data.description}
