977c38c94b40175f40361c88da7182d3c6307e2a
slava-malykhin <slava.malykhin@placense.com>
2021-12-17 09:10:13 +0200

    Update models and telegram message

--- api/front-refresh/telegtam-bot/message.js
+++ api/front-refresh/telegtam-bot/message.js
@@ -5,9 +5,12 @@ module.exports = (data) => `
 Товары:
 ${data.cart_item.map(i => `${i.product.vendor_code}${'.'.repeat(12 - i.product.vendor_code.length)}${i.count}`).join('\n')}
 -------
-Всего: ${data.total_cost} грн
+Сума: ${data.total_cost} грн
+Скидка: ${data.discount} грн
+Сумма со скидкой: ${data.discounted_cost} грн
 
-Пользователь: ${data.user.email}
+Пользователь: ${data.user ? data.user.email : data.user_email}
+${data.user && data.user.is_wholesaler ? 'Оптовик': ''}
 
 Информация о доставке:
 ${data.delivery_info.first_name} ${data.delivery_info.last_name}
